15481ae4cc109fc6d562838a7262c08c617aec89
Sugar <840225521@qq.com>
2020-01-30 13:46:21 +0800

    add index and bootstrap

--- src/main/java/com/lyj/community/controller/IndexController.java
+++ src/main/java/com/lyj/community/controller/IndexController.java
@@ -4,15 +4,13 @@ package com.lyj.community.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String hello(){
+        return "index";
     }
 }
